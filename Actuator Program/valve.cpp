4257ab329452fb3ecdefaa10493c027beb0cdbc5
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-08-30 19:17:06 +0000

    additions

--- Actuator Program/valve.cpp
+++ Actuator Program/valve.cpp
@@ -1,7 +1,23 @@
 #include"../lib/jeeh-fork-master/jee.h"
 
+//Definitions
+// PIN 
+#define PIN_LOC 0
+
+//
+#define OPEN 1
+#define CLOSED 0
+
+PinB<PIN_LOC> currentControl;
+
+//
+
 int main(){
+    currentControl.mode(Pinmode::out);
+
+
+}
+
+void SET_ARMATURE(bool x){
     
-
-
 }
