006aab6ff502f0e43c3294c5fe5677a325d01577
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-10-16 19:29:21 +0000

    added source files (GPIO.h, GPIO.c) for the shift from the jeeh lib

---
+++ Actuator Program/GPIO.h
@@ -0,0 +1,32 @@
+#ifndef ECU_GPIO_
+#define ECU_GPIO_
+
+#include "../stm32/spl/variants/stm32f4/src/stm32f4xx_gpio.c"
+#include "../stm32/cmsis/variants/stm32f4/stm32f4xx.h"
+
+//0x1
+#define CLOCK_A (RCC_AHB1Periph_GPIOA)
+//0x2
+#define CLOCK_B (RCC_AHB1Periph_GPIOB)
+//0x4
+#define CLOCK_C (RCC_AHB1Periph_GPIOC)
+//0x8
+#define CLOCK_D (RCC_AHB1Periph_GPIOD)
+
+#define IN 0
+
+#define OUT 1
+
+//Setup Function
+
+void (SETUP_GPIO)(uint8_t PIN,uint32_t _clock_);
+
+void (SET_ECU_GPIO_HIGH)(uint8_t PIN,uint32_t _clock_);
+
+void (SET_ECU_GPIO_LOW)(uint8_t PIN);
+
+void (SET_GPIO_MODE)(uint8_t PIN,uint8_t mode,uint32_t _clock_);
+
+
+
+#endif //ECU_GPIO
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### About
 
 This project consists of an NC Solenoid Valve Spec controlled and actuated by a STM32 based controller to regulate propellant/fluid flow.
+
+The aim for the valve is to implement the use of the valve to move and regulate variable propellant flow through plumbing systems.
